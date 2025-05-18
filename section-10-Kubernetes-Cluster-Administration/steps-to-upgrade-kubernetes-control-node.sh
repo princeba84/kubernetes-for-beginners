70bac3d17a7d8822388a6891dda6d0faa1b71fde
k8s <k8s@controlplane.example.com>
2025-05-18 12:19:52 +0200

    k8s

--- section-10-Kubernetes-Cluster-Administration/steps-to-upgrade-kubernetes-control-node.sh
+++ section-10-Kubernetes-Cluster-Administration/steps-to-upgrade-kubernetes-control-node.sh
@@ -2,8 +2,8 @@
 
 # Setting up (Changing)  apt repository:
 
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.32/deb/ /' | \
+sudo curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.32/deb/ /' | \
 	sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 
@@ -11,26 +11,26 @@ echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.
 
 # Upgrading kubeadm ,kubelet and kubectl (Execute commands as root user or with sudo)
       
-apt-mark unhold kubeadm kubectl kubelet
-apt-get update   
-apt --fix-broken install
-apt-get install -y kubeadm=1.32.4-1.1 kubectl=1.32.4-1.1 kubelet=1.32.4-1.1  #If it fails , execute apt --fix-broken install 
-apt list -a kubeadm   # To check available versions
-apt-mark hold kubeadm kubectl kubelet
+sudo apt-mark unhold kubeadm kubectl kubelet
+sudo apt-get update   
+sudo apt --fix-broken install -y
+sudo apt-get install -y kubeadm=1.32.4-1.1 kubectl=1.32.4-1.1 kubelet=1.32.4-1.1  #If it fails , execute apt --fix-broken install 
+sudo apt list -a kubeadm   # To check available versions
+sudo apt-mark hold kubeadm kubectl kubelet
        
-kubeadm upgrade plan
-kubeadm upgrade apply v1.32.4 -y
-kubeadm version
+sudo kubeadm upgrade plan
+sudo kubeadm upgrade apply v1.32.4 -y
+sudo kubeadm version
 
 
 # Restart kubelet :
 
-systemctl restart kubelet
-systemctl status kubelet  #To check status after restart
+sudo systemctl restart kubelet
+sudo systemctl status kubelet  #To check status after restart
 
 # Verify version: 
 
-# kubectl get nodes
+kubectl get nodes
 
 
 # In case of HA setup (e.g., 3 control plane nodes ), repeat all steps for other control plane nodes and execute command
